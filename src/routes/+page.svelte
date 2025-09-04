76b96ed89763f3e1d8ebba93fd090a38568c1686
Mikaldr <mikal.drivenes@dof.com>
2025-09-04 14:44:57 +0200

    started work on main pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,58 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    var player = {
+        name: "Mikal",
+        rating: "1100",
+        wins: "9",
+        losses: "1",
+        ranking: "4"
+    }
+</script>
+
+<div class="column">
+
+    <h1>Ping Pong</h1>
+
+    <div class="column profile">
+        <h1>{player.name}</h1>
+        <div class="inline">
+            <h3 class="fade-text">ELO: </h3>
+            <h3> {player.rating} </h3>
+            <h3 class="fade-text">Rank:</h3>
+            <h3
+            class:gold-text={player.ranking === "1"}
+            class:silver-text={player.ranking === "2"}
+            class:bronze-text={player.ranking === "3"}
+            >
+            #{player.ranking}
+            </h3>
+        </div>
+        <div class="inline">
+            <h3>W {player.wins}</h3>
+            <h3>L {player.losses}</h3>
+            <h3>{(player.wins/(player.losses+player.wins)).toFixed(2)}%</h3>
+        </div>
+    </div>
+
+    <div class="button-column">
+        <a class="button" href="/create-match">Create match</a>
+        <a class="button" href="/leaderboard">Leaderboard</a>
+        <a class="button" href="/games">All Games</a>
+        <a class="button" href="/stats">Stats</a>
+    </div>
+</div>
+
+<style>
+    .profile {
+        border-bottom: rgb(223, 223, 223) 2px solid;
+        width: 100%;
+        height: auto;
+        gap: 0;
+        margin-bottom: 2rem;
+    }
+    .profile h3{
+        margin: .5rem;
+    }
+    .profile h1{
+        margin: .5rem;
+    }
+</style>
---
+++ src/routes/create-match/+page.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+  import Select from 'svelte-select';
+
+  type player = {
+    label: string,
+    value: string
+  }
+
+  let players = [
+    { label: "Benjamin", value: "apple" },
+    { label: "Vegard", value: "banana" },
+    { label: "Mikal", value: "orange" }
+  ];
+
+  let selectedDate: string = ""; // ISO date string, e.g. "2025-09-04"
+  let selectedWinner: player | null = null;
+  let selectedLoser: player | null = null;
+</script>
+<div class="column">
+    <h1>Create Match!</h1>
+
+    Match Date
+    <input class="phone-width" type="date" bind:value={selectedDate} />
+    <p>Selected: {selectedDate}</p>
+    Winner
+    <Select
+    class="phone-width"
+    items={players}
+    bind:value={selectedWinner}
+    placeholder="Search or select..."
+    />
+    Winner's Score
+    <input class="phone-width">
+    Loser
+    <Select
+    class="phone-width"
+    items={players}
+    bind:value={selectedLoser}
+    placeholder="Search or select..."
+    />
+    Loser's score
+    <input class="phone-width">
+
+    <a class="button">Create Match</a>
+
+    <a class="button" href="/">Go Back</a>
+</div>
+
+<style>
+    .phone-width {
+        width: 70%;
+    }
+</style>
