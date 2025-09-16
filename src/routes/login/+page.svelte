2d3fb365cceccc5cd93b34af8195ba66477be901
Mikaldr <mikal.drivenes@dof.com>
2025-09-16 12:15:48 +0200

    redid OAuth

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -3,14 +3,10 @@
   import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
   async function loginWithGoogle() {
-    try {
-      const provider = new GoogleAuthProvider();
-      const result = await signInWithPopup(auth, provider);
-      const user = result.user;
-      console.log("Logged in as:", user.displayName, user.email);
-    } catch (err) {
-      console.error("Login failed", err);
-    }
+    if (!auth) return; // SSR safety
+    const provider = new GoogleAuthProvider();
+    const result = await signInWithPopup(auth, provider);
+    console.log(result.user);
   }
 </script>
 
